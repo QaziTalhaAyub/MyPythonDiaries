7a7d250544103ec5b5e65f889f438123b0d4067e
QaziTalhaAyub <110861760+QaziTalhaAyub@users.noreply.github.com>
2022-08-22 08:39:24 +0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 22 08:23:29 2022
+
+@author: user
+"""
+
+# python functions practise///////
+
+
+def greet_user(fist_name, last_name):
+    print(f'Hi{fist_name} {last_name}!')
+    print('Welcome aboard')
+    print('finish')
+    
+    
+    
+print('start')
+greet_user('qazi', last_name = 'danish')
+
+greet_user('qazi', last_name='apple') 
